06aa8a2bcf220a5d2f7a34eabe01f04539d5243c
Aida Miller <Aida@MacBook-Air.local>
2023-11-05 18:24:16 -0700

    added coments

--- src/Module8Final/Main.java
+++ src/Module8Final/Main.java
@@ -5,10 +5,10 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {		
-	  
-	   LinkedList<Vehicle> vehicles = collectVehicles();	
-	  Collections.sort(vehicles);
-	  saveVehiclesToFile(vehicles);
+	   // Collect vehicle information
+	  LinkedList<Vehicle> vehicles = collectVehicles();	
+	  Collections.sort(vehicles); // Sort the list of vehicles
+	  saveVehiclesToFile(vehicles);// Save the sorted vehicle information to a file
 	}
 	
 	private static LinkedList<Vehicle>  collectVehicles() {
@@ -31,9 +31,10 @@ public class Main {
 			   model = scanner.nextLine();
 			   
 			   System.out.print("Miles per gallon: ");
+			   // Check if the input is a valid double for miles per gallon
 			   milesPerGallon = possibleMilesPerGallon(scanner.nextLine());	
 			   
-			   
+			   // Create a Vehicle object and add it to the LinkedList
 			   Vehicle vehicle = new Vehicle(make, model, milesPerGallon);
 				  vehicles.add(vehicle);
 //				  scanner.nextLine();
@@ -57,6 +58,7 @@ public class Main {
 		
 		try {
 			writer.write("Make\t\tModel\t\t MPG \n");
+			 // Write each vehicle's information to the file
 			for (Vehicle car: vehicles) {
 				writer.write(car.getMake()+"\t\t"+car.getModel()+"\t\t"+car.getMilesPerGallon()+"\n");
 			} 
@@ -69,8 +71,10 @@ public class Main {
 		try {
 			return Double.parseDouble(possibleDouble);
 		} catch (NumberFormatException ex){
+			// If the input is not a valid number, prompt the user to enter a number
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("Try again.That was not a number.");
+			// Recursively call the method until a valid number is entered
 			return possibleMilesPerGallon(scanner.nextLine());
 		}
 	}
